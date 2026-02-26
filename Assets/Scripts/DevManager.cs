9aa588139f75a7267cdcc92d2a3667fd579ae978
Jesse Vu <jessevu@gmail.com>
2026-02-26 13:48:11 -0500

    dev mode toggle w/ add waypoint button

---
+++ Assets/Scripts/DevManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DevManager : MonoBehaviour
+{
+    [Header("Dev Mode Settings")]
+    public bool devModeEnabled = false;
+    public InputActionProperty devToggleAction;
+
+    public Transform playerCamera;
+
+    void Start()
+    {
+        if (devModeEnabled)
+        {
+            devToggleAction.action.Enable();
+            devToggleAction.action.performed += _ => SpawnWaypoint();
+        }
+    }
+
+    void SpawnWaypoint()
+    {
+        Vector3 pos = playerCamera != null ? playerCamera.position : Vector3.zero;
+        WaypointManager.Instance.CreateWaypoint(new Waypoint(pos));
+        Debug.Log($"Dev waypoint created at {pos}");
+    }
+}
